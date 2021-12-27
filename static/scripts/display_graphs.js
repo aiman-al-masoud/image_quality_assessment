fe83ceb58e7ed494452aaa4ca297cb837c99d804
aiman-al-masoud <luxlunarislabs@gmail.com>
2021-12-27 18:34:29 +0400

    fixed undeclared curve in display_graphs.js

--- static/scripts/display_graphs.js
+++ static/scripts/display_graphs.js
@@ -1,28 +1,17 @@
 function display(names, originals, imp1, imp2, div_name, title) {
-    console.log(names);
-    console.log(originals);
-    console.log(imp1);
-    var original_curve = {
-        x: names,
-        y: originals,
-        type: 'scatter',
-        name: "Orginal Images",
-        marker: {
-          color: '#44d800',
-          size: 12
-        }
-      };
-      
-      var impairment_1_curve = {
-        x: names,
-        y: imp1,
-        type: 'scatter',
-        name: "Impairment 1 Images",
-        marker: {
-          color: 'orange',
-          size: 12
-        }
-      };
+  console.log(names);
+  console.log(originals);
+  console.log(imp1);
+  var original_curve = {
+      x: names,
+      y: originals,
+      type: 'scatter',
+      name: "Orginal Images",
+      marker: {
+        color: '#44d800',
+        size: 12
+      }
+    };
 
     var impairment_1_curve = {
       x: names,
@@ -35,57 +24,68 @@ function display(names, originals, imp1, imp2, div_name, title) {
       }
     };
 
-      var layout = {
-        paper_bgcolor: '#080808',
-        plot_bgcolor: '#080808',
-        xaxis: {
-          title: 'Image Name',
-          color: 'rgb(241, 241, 241)',
-          gridcolor: '#444444' 
-        },
-        yaxis: {
-          title: title,
-          color: 'rgb(241, 241, 241)',
-          gridcolor: '#444444'
-        },
-        legend: {
-          font: {
-            color: 'rgb(241, 241, 241)'  
-          }
-        }
-      };
+  var impairment_2_curve = {
+    x: names,
+    y: imp1,
+    type: 'scatter',
+    name: "Impairment 2 Images",
+    marker: {
+      color: 'red',
+      size: 12
+    }
+  };
 
-      var data = [original_curve, impairment_1_curve, impairment_2_curve];
-      
-      Plotly.newPlot(div_name, data, layout);
+    var layout = {
+      paper_bgcolor: '#080808',
+      plot_bgcolor: '#080808',
+      xaxis: {
+        title: 'Image Name',
+        color: 'rgb(241, 241, 241)',
+        gridcolor: '#444444'
+      },
+      yaxis: {
+        title: title,
+        color: 'rgb(241, 241, 241)',
+        gridcolor: '#444444'
+      },
+      legend: {
+        font: {
+          color: 'rgb(241, 241, 241)'
+        }
+      }
+    };
+
+    var data = [original_curve, impairment_1_curve, impairment_2_curve];
+
+    Plotly.newPlot(div_name, data, layout);
 }
 
 window.onload = () => {
-  const url = 'https://siqa.pythonanywhere.com';
-  //const url = 'http://localhost:5000';
-  fetch(url + '/get-mos-data')
-  .then((response) => {
-      return response.json();
-  })
-  .then(json_data => {
-      let names = json_data["names"];
-      let originals = json_data["original-image-mos"];
-      let imp1 = json_data["slightly-impaired-image-mos"];
-      let imp2 = json_data["heavily-impaired-image-mos"];
+const url = 'https://siqa.pythonanywhere.com';
+//const url = 'http://localhost:5000';
+fetch(url + '/get-mos-data')
+.then((response) => {
+    return response.json();
+})
+.then(json_data => {
+    let names = json_data["names"];
+    let originals = json_data["original-image-mos"];
+    let imp1 = json_data["slightly-impaired-image-mos"];
+    let imp2 = json_data["heavily-impaired-image-mos"];
 
-      display(names, originals, imp1, imp2, "mos_graph_div", "MOS");
+    display(names, originals, imp1, imp2, "mos_graph_div", "MOS");
+});
+
+fetch(url + '/get-stddev-mos-data')
+.then((response) => {
+    return response.json();
+})
+.then(json_data => {
+    let names = json_data["names"];
+    let originals = json_data["original-image-stddev-mos"];
+    let imp1 = json_data["slightly-impaired-image-stddev-mos"];
+    let imp2 =json_data["heavily-impaired-image-stddev-mos"];
+
+      display(names, originals, imp1, imp2, "stddev_mos_graph_div", "&#963;");
   });
-
-  fetch(url + '/get-stddev-mos-data')
-  .then((response) => {
-      return response.json();
-  })
-  .then(json_data => {
-      let names = json_data["names"];
-      let originals = json_data["original-image-stddev-mos"];
-      let imp1 = json_data["slightly-impaired-image-stddev-mos"];
-      let imp2 =json_data["heavily-impaired-image-stddev-mos"];
-
-        display(names, originals, imp1, imp2, "stddev_mos_graph_div", "&#963;");
-    });
 }
