b8bb3baaabafbf6747c323b9f55ad3f45ac9b093
TonyStark9 <38134646+TonyStark9@users.noreply.github.com>
2021-12-27 12:40:58 +0100

    solved display graph issues

--- static/scripts/display_graphs.js
+++ static/scripts/display_graphs.js
@@ -1,4 +1,7 @@
 function display(names, originals, imp1, imp2, div_name) {
+    console.log(names);
+    console.log(originals);
+    console.log(imp1);
     var original_curve = {
         x: names,
         y: originals,
@@ -23,25 +26,31 @@ function display(names, originals, imp1, imp2, div_name) {
 }
 
 window.onload = () => {
-    fetch('https://siqa.pythonanywhere.com/get-mos-data')
+    //const url = 'https://siqa.pythonanywhere.com';
+    const url = 'http://localhost:5000';
+    fetch(url + '/get-mos-data')
     .then((response) => {
-        let names = response.json()["names"];
-        let originals = response.json()["original-image-mos"];
-        let imp1 = response.json()["slightly-impaired-image-mos"];
-        let imp2 = response.json()["heavily-impaired-image-mos"];
+        return response.json();
+    })
+    .then(json_data => {
+        let names = json_data["names"];
+        let originals = json_data["original-image-mos"];
+        let imp1 = json_data["slightly-impaired-image-mos"];
+        let imp2 = json_data["heavily-impaired-image-mos"];
 
         display(names, originals, imp1, imp2, "mos_graph_div");
-    })
-    .then(data => console.log(data));
+    });
 
-    fetch('https://siqa.pythonanywhere.com/get-stddev-mos-data')
+    fetch(url + '/get-stddev-mos-data')
     .then((response) => {
-        let names = response.json()["names"];
-        let originals = response.json()["original-image-stddev-mos"];
-        let imp1 = response.json()["slightly-impaired-image-stddev-mos"];
-        let imp2 = response.json()["heavily-impaired-image-stddev-mos"];
+        return response.json();
+    })
+    .then(json_data => {
+        let names = json_data["names"];
+        let originals = json_data["original-image-stddev-mos"];
+        let imp1 = json_data["slightly-impaired-image-stddev-mos"];
+        let imp2 =json_data["heavily-impaired-image-stddev-mos"];
 
         display(names, originals, imp1, imp2, "stddev_mos_graph_div");
-    })
-    .then(data => console.log(data));
+    });
 }
